981f26c33538697294a9007f6078ba29844ac01b
gusss666 <114181785+gusss666@users.noreply.github.com>
2022-11-30 19:45:57 -0300

    Add files via upload

---
+++ exericio1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char palavra1[50];
+    char palavra2[50];
+    int i;
+    
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra1);
+    
+    for (i=0;i<50;i++)
+    {
+        palavra2 [i] = palavra1 [i];
+        
+    }
+    
+    printf("\nSua palavra copiada: ");
+    printf("%s.", palavra2);
+    return 0;
+}
